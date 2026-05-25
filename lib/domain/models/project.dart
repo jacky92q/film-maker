6507e04ba05646e930acf4373fd640177a0f6e22
jacky92q <76251846+jacky92q@users.noreply.github.com>
2026-05-25 23:48:29 +0900

    Update project.dart

---
+++ lib/domain/models/project.dart
@@ -0,0 +1,11 @@
+class Project {
+  const Project({
+    required this.id,
+    required this.title,
+    required this.sceneCount,
+  });
+
+  final String id;
+  final String title;
+  final int sceneCount;
+}
