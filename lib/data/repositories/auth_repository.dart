0d6a870860bf8fbe363843afa20454d91dddecb3
jacky92q <76251846+jacky92q@users.noreply.github.com>
2026-05-26 00:01:38 +0900

    Update auth_repository.dart

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,1 +1,13 @@
+import 'package:film_maker/data/services/mock_auth_service.dart';
 
+class AuthRepository {
+  AuthRepository({required MockAuthService authService}) : _authService = authService;
+
+  final MockAuthService _authService;
+
+  Future<bool> login({required String email, required String password}) async {
+    await _authService.login(email: email, password: password);
+    return true;
+  }
+
+}
